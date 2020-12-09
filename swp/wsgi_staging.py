3051db1065584fa4554f859c79257eb5a968260b
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-12-09 15:07:33 -0300

    Update wsgi_staging.py

--- swp/wsgi_staging.py
+++ swp/wsgi_staging.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.staging')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swp.settings.staging')
 
 application = get_wsgi_application()
